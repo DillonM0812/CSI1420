41f4a7bb62de49eec9a621c3e5732303909d3d42
Sacoren65 <Sacoren65@gmail.com>
2023-11-13 17:14:26 -0500

    Update main.c
    I'm going to update this. I'm not sure whether it has any errors, but I want to make sure it's up for ease of access.

--- main.c
+++ main.c
@@ -81,6 +81,25 @@ int main() {
 
     //Do stop word removal, sorting, saving to file, etc.
 
+	//This section is for setting up the tokenized file string array
+	printf("Test. Start of tokized file writing.\n");
+	FILE *tf;
+	char *Tfilepaths[NUMFILES];
+		Tfilepaths[0] = "TokenizedFile1.txt";
+		Tfilepaths[1] = "TokenizedFile2.txt";
+		Tfilepaths[2] = "TokenizedFile3.txt";
+		Tfilepaths[3] = "TokenizedFile4.txt";
+	
+	//this for loop will open a file for writing, add the tokenized string to the file, and then close that file.
+	for(int i = 0; i < NUMFILES; i++)
+	{
+		printf("Begin opening of file for writing.\n");
+		tf = fopen(Tfilepaths[i], "w");
+		fprintf(tf, "%s", files[i].tokens);
+		fclose(tf);
+		
+	}
+	
     //Memory cleanup, all code goes above this
     for(int i = 0; i < NUMFILES; i++) {
         free(files[i].data);
